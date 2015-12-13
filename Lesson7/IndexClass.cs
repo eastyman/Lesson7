de544a068503e0e90869f09abec9e7a7423f03da
eastyman <bra4o.il@gmail.com>
2015-12-13 12:44:01 +0200

    1

---
+++ Lesson7/IndexClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7
+{
+   class Index
+   {
+       public string Name { get; set; }
+       public Index(string name)
+       {
+           Name = name;
+       }
+
+}
+    class IndexClass
+    {
+        private Index[] indexes;
+        public IndexClass(int length)
+        {
+            indexes = new Index[length];
+        }
+       public int Length
+       {
+          get { return indexes.Length; }
+       }
+        public Index this[int index]
+        {
+            get
+            {
+                return indexes[index];
+            }
+            set
+            {
+                indexes[index] = value;
+
+            } 
+        }
+    }
+    
+}
